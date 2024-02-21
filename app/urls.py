e90db63670b96a631a1b684797b54ace2fcae368
olavolicursi <69656251+olavolicursi@users.noreply.github.com>
2024-02-21 17:06:40 -0300

    urls accounts

--- app/urls.py
+++ app/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/', include('authentication.urls')),
+    path('api/v1/', include('accounts.urls')),
 ]
