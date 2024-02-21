411ee1872679f4992302302439386af6ecc9a57c
olavolicursi <69656251+olavolicursi@users.noreply.github.com>
2024-02-21 17:03:50 -0300

    first commit

---
+++ accounts/views.py
@@ -0,0 +1,15 @@
+from rest_framework import generics
+from accounts.models import Account
+from accounts.serializers import AccountSerializer
+
+
+class AccountCreateListView(generics.ListCreateAPIView):
+    #permission_classes = (IsAuthenticated, GlobalDefaultPermission,)
+    queryset = Account.objects.all()
+    serializer_class = AccountSerializer
+
+
+class AccountRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    #permission_classes = (IsAuthenticated, GlobalDefaultPermission,)
+    queryset = Account.objects.all()
+    serializer_class = AccountSerializer
