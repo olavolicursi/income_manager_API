392409283460604ff7e077b037050e0da3c72864
olavolicursi <69656251+olavolicursi@users.noreply.github.com>
2024-03-13 09:02:41 -0300

    uncomment

--- transactions/serializers.py
+++ transactions/serializers.py
@@ -19,20 +19,3 @@ class TransactionListDetailSerializer(serializers.ModelSerializer):
         model = Transaction
         fields = ['id', 'account_id', 'value', 'emission_date', 'payment_date', 'due_date', 'time', 'type',
                   'description', 'category_id']
-
-    # def create(self, validated_data):
-    #     # Remova 'account' do validated_data para evitar o erro
-    #     account = validated_data.pop('account', None)
-
-    #     # Crie a transação
-    #     transaction = Transaction.objects.create(**validated_data)
-
-    #     # Se 'user' estiver presente, ajuste o saldo do usuário
-    #     if account:
-    #         if type == "D":
-    #             account.balance -= transaction.value
-    #         elif type == "C":
-    #             account.balance += transaction.value
-    #         account.save()
-
-    #     return transaction
--- transactions/signals.py
+++ transactions/signals.py
@@ -1,8 +1,6 @@
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 from django.utils import timezone
-# from celery import shared_task
-# from accounts.models import Account
 from transactions.models import Transaction
 
 
@@ -26,33 +24,3 @@ def delete_account_balance(sender, instance, **kwargs):
     elif instance.type == "C":
         account_id.balance -= instance.value
     account_id.save()
-
-    # @shared_task
-# def update_account_balance_async(account, value, type):
-#     account = Account.objects.get(pk=account)
-#     if type == "D":
-#         account.balance -= value
-#     elif type == "C":
-#         account.balance += value
-#     account.save()
-
-# @receiver(post_save, sender=Transaction)
-# def schedule_update_account_balance(sender, instance, **kwargs):
-#     account = instance.account
-#     value = instance.value
-#     type = instance.type
-
-#     # Obtenha a data de pagamento da instância
-#     payment_date = instance.payment_date
-
-#     # Converta a data de pagamento para datetime se for uma data
-#     if isinstance(payment_date, timezone.datetime):
-#         payment_date = payment_date.date()
-
-#     # Verificar se a data de pagamento é futura
-#     if payment_date > timezone.now().date():
-#         # Agendar a tarefa para a data de pagamento
-#         update_account_balance_async.apply_async(args=[account, value, type], eta=payment_date)
-#     else:
-#         # A data de pagamento é hoje ou passada, executar imediatamente
-#         update_account_balance_async(account, value, type)
